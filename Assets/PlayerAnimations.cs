0b9c62c02ae57352819113400ce9af5b0f51ec26
Roey_Aharonov <roeyah@matrixdna.ai>
2023-05-08 02:01:42 +0300

    change sprite and create simple animator + script

---
+++ Assets/PlayerAnimations.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class PlayerAnimations : MonoBehaviour
+{
+    [SerializeField] Animator _animator;
+    [SerializeField] SpriteRenderer _spriteRenderer;
+    // Start is called before the first frame update
+    void Start()
+    {
+        _animator = GetComponent<Animator>();
+        _spriteRenderer = GetComponent<SpriteRenderer>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float horizontalAxis = Input.GetAxis("Horizontal");
+        float verticalAxis = Input.GetAxis("Vertical");
+
+        bool isWalking = (horizontalAxis != 0 || verticalAxis != 0);
+
+        if (horizontalAxis > 0)
+        {
+            _spriteRenderer.flipX = true;
+        }
+        if (horizontalAxis < 0)
+        {
+            _spriteRenderer.flipX = false;
+        }
+
+        var state = isWalking ? Animator.StringToHash("NoctisWalk") : Animator.StringToHash("IdleNoctis");
+        //print(horizontalAxis != 0 || verticalAxis != 0);
+
+        //_animator.SetBool("isWalking", horizontalAxis != 0 || verticalAxis != 0);
+        _animator.CrossFade(state, 0);
+    }
+}
